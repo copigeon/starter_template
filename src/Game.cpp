a4b3be431ea2a389509442e8218bbe17729c8a21
copigeon <146360316+copigeon@users.noreply.github.com>
2024-10-09 20:09:53 +0100

    narwhal

--- src/Game.cpp
+++ src/Game.cpp
@@ -10,34 +10,80 @@ Game::Game(sf::RenderWindow& game_window)
 
 Game::~Game()
 {
-
+	delete character;
+	delete passport;
+	delete [] passports;
+	delete [] animals;
 }
 
 bool Game::init()
 {
+	character = new sf::Sprite;
+	passport = new sf::Sprite;
+
 	if (!background_texture.loadFromFile("../Data/WhackaMole Worksheet/background.png"))
 	{
 		std::cout << "background texture did not load \n";
 	}
 	background.setTexture(background_texture);
 
-	if (!bird_texture.loadFromFile("../Data/WhackaMole Worksheet/bird.png"))
-	{
-		std::cout << "bird texture did not load \n";
-	}
-	bird.setTexture(bird_texture);
-	bird.setPosition(200, 200);
-	bird.setScale(0.5,0.5);
+	//for (int i = 0; i <= 2; i++) {
+		if (!passports[0].loadFromFile("../Data/Images/Critter/moose.png"))
+		{
+			std::cout << "moose texture did not load \n";
+		}
+		if (!passports[1].loadFromFile("../Data/Images/Critter/narwhal.png"))
+		{
+			std::cout << "narwhal texture did not load \n";
+		}
+		if (!passports[2].loadFromFile("../Data/Images/Critter/penguin.png"))
+		{
+			std::cout << "penguin texture did not load \n";
+		}
+
+		if (!animals[0].loadFromFile("../Data/Images/Critter/moose.png"))
+		{
+			std::cout << "moose texture did not load \n";
+		}
+		if (!animals[1].loadFromFile("../Data/Images/Critter/narwhal.png"))
+		{
+			std::cout << "narwhal texture did not load \n";
+		}
+		if (!animals[2].loadFromFile("../Data/Images/Critter/penguin.png"))
+		{
+			std::cout << "penguin texture did not load \n";
+		}
+	//}
 
 	if (!myfont.loadFromFile("../Data/Fonts/OpenSans-Bold.ttf"))
 	{
 		std::cout << "myfont did not load \n";
 	}
+	logo.setString("WOOO!");
 	logo.setFont(myfont);
 	logo.setCharacterSize(50);
 	logo.setFillColor(sf::Color(sf::Color::Red));
-	logo.setPosition((window.getSize().x / 2) - logo.getGlobalBounds().width, 100);
-	logo.setString("WOOO!");
+	logo.setPosition((window.getSize().x / 2) - (logo.getGlobalBounds().width/2), 100);
+
+	welcome.setString("Welcome to the game!");
+	welcome.setFont(myfont);
+	welcome.setCharacterSize(50);
+	welcome.setFillColor(sf::Color(sf::Color::Yellow));
+	welcome.setPosition((window.getSize().x / 2) - (welcome.getGlobalBounds().width/2), 100);
+
+	play.setString("play");
+	play.setFont(myfont);
+	play.setCharacterSize(50);
+	play.setFillColor(sf::Color(sf::Color::Yellow));
+	play.setPosition((window.getSize().x / 2) - 200, 250);
+
+	quit.setString("quit");
+	quit.setFont(myfont);
+	quit.setCharacterSize(50);
+	quit.setFillColor(sf::Color(sf::Color::Yellow));
+	quit.setPosition((window.getSize().x / 2) + 200, 250);
+
+
 
   return true;
 }
@@ -49,9 +95,16 @@ void Game::update(float dt)
 
 void Game::render()
 {
-	window.draw(background);
-	window.draw(bird);
-	window.draw(logo);
+	if (in_menu) {
+		window.draw(welcome);
+		window.draw(play);
+		window.draw(quit);
+	}
+	else {
+		window.draw(background);
+		//window.draw(character);
+		window.draw(logo);
+	}
 }
 
 void Game::mouseClicked(sf::Event event)
@@ -64,7 +117,7 @@ void Game::mouseClicked(sf::Event event)
 
 void Game::keyPressed(sf::Event event)
 {
-
+	if (event.key.code == sf::Keyboard::P) { in_menu = false; }
 }
 
 
--- src/Game.h
+++ src/Game.h
@@ -6,25 +6,31 @@
 
 class Game
 {
- public:
-  Game(sf::RenderWindow& window);
-  ~Game();
-  bool init();
-  void update(float dt);
-  void render();
-  void mouseClicked(sf::Event event);
-  void keyPressed(sf::Event event);
+public:
+	Game(sf::RenderWindow& window);
+	~Game();
+	bool init();
+	void update(float dt);
+	void render();
+	void mouseClicked(sf::Event event);
+	void keyPressed(sf::Event event);
+	bool in_menu = true;
 
- private:
-  sf::RenderWindow& window;
-  sf::Sprite ball;
-  sf::Texture ball_texture;
-  sf::Sprite background;
-  sf::Texture background_texture;
-  sf::Sprite bird;
-  sf::Texture bird_texture;
-  sf::Text logo;
-  sf::Font myfont;
+private:
+	sf::RenderWindow& window;
+	sf::Sprite ball;
+	sf::Texture ball_texture;
+	sf::Sprite background;
+	sf::Texture background_texture;
+	sf::Sprite* character;
+	sf::Texture* animals = new sf::Texture[3];
+	sf::Sprite* passport;
+	sf::Texture* passports = new sf::Texture[3];
+	sf::Text logo;
+	sf::Text welcome;
+	sf::Font myfont;
+	sf::Text play;
+	sf::Text quit;
 
 };
 
